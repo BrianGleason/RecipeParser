9415d517a2b7ae354d2784e48205754762b8191d
Carson Surmeier <carson@surmeier.us>
2022-06-02 18:01:24 -0500

    Add database, update food classifier

--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -1,3 +1,10 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["cooking_data"]
+recipies = db["recipie_data"]
+diets = db["diet_data"]
+
 import nltk
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
@@ -12,61 +19,82 @@ from pprint import pprint
 import sys
 
 def parse_recipie(url):
+    """Crawl url and insert recipie data into database collection.
+    """
+    req = urllib.request.Request(url)
+    req.add_header('Cookie', 'euConsent=true')
+    soup = BeautifulSoup(urllib.request.urlopen(req).read(), 'html.parser')
 
-	req = urllib.request.Request(url)
-	req.add_header('Cookie', 'euConsent=true')
-	soup = BeautifulSoup(urllib.request.urlopen(req).read(), 'html.parser')
+    title = soup.find('title').string
+    ingredient_tags = soup.findAll("li", {"class": "ingredients-item"})
+    instruction_tags = soup.findAll("li", {"class": "subcontainer instructions-section-item"})
 
-	ingredient_tags = soup.findAll("li", {"class": "ingredients-item"})
-	instruction_tags = soup.findAll("li", {"class": "subcontainer instructions-section-item"})
+    ingredients = []
+    for tag in ingredient_tags:
+        ingredient = {}
 
-	ingredients = []
-	for tag in ingredient_tags:
-		ingredient = {}
+        try:
+            ingredient['name'], ingredient['prep'] = tag.findChild("input")['data-ingredient'].split(",")
+        except ValueError:
+            ingredient['name'] = tag.findChild("input")['data-ingredient']
+            ingredient['prep'] = ''
 
-		try:
-			ingredient['name'], ingredient['prep'] = tag.findChild("input")['data-ingredient'].split(",")
-		except ValueError:
-			ingredient['name'] = tag.findChild("input")['data-ingredient']
-			ingredient['prep'] = ''
+        ingredient['quantity'] = tag.findChild("input")['data-init-quantity']
+        ingredient['unit'] = tag.findChild("input")['data-unit']
+        ingredient['type'] = tag.findChild("input")['data-store_location']
+        ingredients.append(ingredient)
 
-		ingredient['quantity'] = tag.findChild("input")['data-init-quantity']
-		ingredient['unit'] = tag.findChild("input")['data-unit']
-		ingredient['type'] = tag.findChild("input")['data-store_location']
-		ingredients.append(ingredient)
+    instructions = []
+    for tag in instruction_tags:
+        instructions.append(tag.find('p').getText())
 
-	instructions = []
-	for tag in instruction_tags:
-		instructions.append(tag.find('p').getText())
+    recipie = {
+        'Name': title,
+        'Ingredients': ingredients,
+        'Instructions': instructions
+    }
+    pprint(recipie)
 
-	pprint(ingredients)
-	pprint(instructions)
-	return ingredients, instructions
+    # TODO: Duplicate detection
+    recipies.insert_one(recipie)
 
-def food_diet(ingredient):
-	# TODO: return all deitary restrictions category allows, healthy/non-healthy instead of keyword
-	# See examples below.
-	# There are useful and non-useful categories. "Soup" is not useful because soup can contain meat or gluten,
-	# i.e. "beef broth" is not vegitarian
+def food_classifier(ingredient):
+    # Hopefully all the hardcoded garbage can go in this function.
+    # There are useful and non-useful categories. "Soup" is not useful because soup can contain meat or gluten,
+    # i.e. "beef broth" is not vegitarian
 
-	if ingredient["type"] == "Beverages":
-		return "beverage"
-	elif ingredient["type"] == "Produce":
-		return "vegan"
-	elif ingredient["type"] == "Herbs and Spices":
-		return "condiment"
-	elif ingredient["type"] == "Meats, Fish and Seafood":
-		return "non-vegan"
-	elif ingredient["type"] == "Basic Cooking Ingredients":
-		return "condiment"
-	elif ingredient["type"] == "Dairy, Eggs and Milk":
-		return "non-vegan"
-	elif ingredient["type"] == "Baking Supplies":
-		return "glutenous"
-	elif ingredient["type"] == "Canned Foods":
-		return None
-	elif ingredient["type"] == "Soup":
-		return None
+    diet = {
+        'Name': ingredient['name'],
+        'Contains' : {'Meat': None, 'Gluten': None, 'Lactose': None},
+        'Healthy' : 2
+    }
+
+    match ingredient['type']:
+        case 'Herbs and Spices' | 'Produce':
+            diet['Contains']['Meat'] = False
+            diet['Contains']['Gluten'] = False
+            diet['Contains']['Lactose'] = False
+            diet['Healthy'] = 3
+        case 'Beverages':
+            diet['Contains']['Meat'] = False
+            diet['Contains']['Gluten'] = False
+        case 'Meats, Fish and Seafood':
+            diet['Contains']['Meat'] = True
+            diet['Contains']['Gluten'] = False
+            diet['Contains']['Lactose'] = False
+            diet['Healthy'] = 1
+        case 'Dairy, Eggs and Milk':
+            diet['Contains']['Meat'] = False
+            diet['Contains']['Gluten'] = False
+            diet['Contains']['Lactose'] = True
+            diet['Healthy'] = 1
+        case 'Baking Supplies':
+            diet['Contains']['Meat'] = False
+            diet['Contains']['Gluten'] = True
+            diet['Contains']['Lactose'] = True
+    # Basic Cooking Ingredients, Canned Foods, Soup indicate nothing
+
+    diets.insert_one(diet)
 
 def quantity_mod(ingredients, ratio):
     for ingredient in ingredients:
@@ -86,4 +114,4 @@ def cooking_method(instruction):
 
 # Testing functions
 if __name__ == '__main__':
-	globals()[sys.argv[1]](sys.argv[2])
+    globals()[sys.argv[1]](sys.argv[2])
