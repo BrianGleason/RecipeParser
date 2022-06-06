67873d6c1ffc6bec2d4e7c5e737c5d16d8654bb3
Caden Howell <cadenhowell0322@gmail.com>
2022-06-06 15:57:57 -0500

    Protein Identifiation

---
+++ src/proteins.py
@@ -0,0 +1,57 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+# Find meats fish and eggs
+URL = 'https://www.listchallenges.com/160-types-of-meat-what-kind-of-meat-have-you-tried/list/'
+proteins = {}
+proteins['meat-fish-eggs'] = ['Egg']
+for i in range(1, 5):
+    page = requests.get(URL + str(i))
+    soup = BeautifulSoup(page.content, 'html.parser')
+    divs = soup.find_all(class_ = "item-name")
+    for div in divs:
+        proteins['meat-fish-eggs'].append(div.text.strip())
+
+# Find dairy products
+URL = 'https://en.wikipedia.org/wiki/List_of_dairy_products'
+proteins['dairy'] = []
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+dairies = soup.select('tbody tr td:nth-of-type(1) > a')
+for dairy in dairies:
+    proteins['dairy'].append(dairy.text.strip())
+
+# Find Seeds and Nuts
+URL = 'https://www.adducation.info/mankind-nature-general-knowledge/edible-nuts-and-seeds/'
+proteins['nuts-seeds'] = []
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+nuts = soup.select('tbody tr td:nth-of-type(1) strong:nth-of-type(1)')
+for nut in nuts:
+    proteins['nuts-seeds'].append(nut.text.strip())
+
+# Find Legumes
+URL = 'https://nutritionrefined.com/legumes-list/'
+legume_set = set()
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+legumes = soup.select('.entry-content h4')
+for legume in legumes:
+    str = legume.text.strip()
+    if str == '': continue
+    if '(' in str:
+        legume_split = str.split('(')
+        legume_name = legume_split[0].strip()
+        legume_subnames = legume_split[1].strip(')')
+        legume_sublist = legume_subnames.split(',')
+        legume_set.add(legume_name)
+        for subname in legume_sublist:
+            legume_set.add(subname.strip())
+    else:
+        legume_set.add(str)
+proteins['legumes'] = list(legume_set)
+
+with open('../lists/formatted_proteins_list.json', 'w') as f:
+    json.dump(proteins, f, ensure_ascii=False)
