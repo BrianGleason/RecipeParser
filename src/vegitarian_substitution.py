9f52318108292cd3d9c39a16d45b6b400025d7ac
Caden Howell <cadenhowell0322@gmail.com>
2022-06-08 15:09:16 -0500

    Vegetarian to/from

--- src/vegitarian_substitution.py
+++ src/vegitarian_substitution.py
@@ -1,28 +1,26 @@
-import os 
 import json
-import sys
-from utils.utilities import parse_recipe, replace_ingredient, replace_ingredient_list, print_recipe
+import os
+import random
 
-def main():
-    protein_data = open(os.path.dirname(__file__) + f'../lists/formatted_proteins_list.json')
-    protein_dict = json.load(protein_data)
-    meats = protein_dict['meat-fish']
+from utils.utilities import replace_ingredient, replace_ingredient_list
 
-    args = sys.argv
-    print(args)
-    if len(args) != 2:
-        print('must provide 1 additional arguments: allrecipes url')
-        return
-    url = args[1]
 
-    recipe = parse_recipe(url)
+def to_vegetarian(recipe):
     ingredients = recipe['Ingredients']
     instructions = recipe['Instructions']
     for ingredient in ingredients:
         if ingredient['contains']['Meat']:
             replace_ingredient(instructions, ingredient['name'], 'tofu')
             replace_ingredient_list(ingredients, ingredient['name'], 'tofu')
-    print_recipe
 
-if __name__ == '__main__':
-    main()
+def from_vegetarian(recipe):
+    protein_data = open(os.path.dirname(__file__) + f'/../lists/formatted_proteins_list.json')
+    protein_dict = json.load(protein_data)
+    meats = protein_dict['meat-fish']
+    ingredients = recipe['Ingredients']
+    instructions = recipe['Instructions']
+    for ingredient in ingredients:
+        if ingredient['type'] == 'Natural/Organic Foods':
+            meat = random.choice(meats)
+            replace_ingredient(instructions, ingredient['name'], meat)
+            replace_ingredient_list(ingredients, ingredient['name'], meat)
