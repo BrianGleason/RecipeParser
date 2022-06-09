1da6a3c536e0ae469f0d6f2d2ec7341a1599b4b0
Carson Surmeier <carson@surmeier.us>
2022-06-09 18:34:47 -0500

    Add loop

--- src/interface.py
+++ src/interface.py
@@ -26,43 +26,51 @@ while True:
 recipe = parse_recipe(url)
 print_recipe(recipe, termsize, 'green', 'red')
 
-print(colored("What transformation do you want to perform? Avalable options:".center(termsize),'green'))
-print("To vegitarian (Enter 1)")
-print("To non-vegitarian (Enter 2)")
-print("To Italian (Enter 3)")
-print("To healthy (Enter 4)")
-print("To unhealthy (Enter 5)")
-print("Change serving amount (Enter 6)")
-print("Change cooking method (Enter 7)")
-print("To lactose-free (Enter 8)")
-
 while True:
-    try:
-        transform = int(input('Your choice (1-8): '))
-        assert 0 < transform < 9
+    print(colored("What transformation do you want to perform? Avalable options:".center(termsize),'green'))
+    print("To vegitarian (Enter 1)")
+    print("To non-vegitarian (Enter 2)")
+    print("To Italian (Enter 3)")
+    print("To healthy (Enter 4)")
+    print("To unhealthy (Enter 5)")
+    print("Change serving amount (Enter 6)")
+    print("Change cooking method (Enter 7)")
+    print("To lactose-free (Enter 8)")
 
-    except ValueError:
-        print("Please enter an integer.")
-    except AssertionError:
-        print("Please enter an integer 1-8")
-    else:
-        break
+    while True:
+        try:
+            transform = int(input('Your choice (1-8): '))
+            assert 0 < transform < 9
 
-if transform == 1:
-    to_vegetarian(recipe)
-elif transform == 2:
-    from_vegetarian(recipe)
-elif transform == 3:
-    italian_cuisine_substitution(recipe, url)
-elif transform == 4:
-    healthy_conversion(recipe, "healthy", termsize)
-elif transform == 5:
-    healthy_conversion(recipe, "unhealthy", termsize)
-elif transform == 6:
-    serving_size(recipe, termsize)
-elif transform == 7:
-    method_interface(recipe, termsize)
-elif transform == 8:
-    make_dairy_free(recipe)
+        except ValueError:
+            print("Please enter an integer.")
+        except AssertionError:
+            print("Please enter an integer 1-8")
+        else:
+            break
 
-print_recipe(recipe, termsize, 'green', 'blue')
+    if transform == 1:
+        to_vegetarian(recipe)
+    elif transform == 2:
+        from_vegetarian(recipe)
+    elif transform == 3:
+        italian_cuisine_substitution(recipe, url)
+    elif transform == 4:
+        healthy_conversion(recipe, "healthy", termsize)
+    elif transform == 5:
+        healthy_conversion(recipe, "unhealthy", termsize)
+    elif transform == 6:
+        serving_size(recipe, termsize)
+    elif transform == 7:
+        method_interface(recipe, termsize)
+    elif transform == 8:
+        make_dairy_free(recipe)
+
+    print_recipe(recipe, termsize, 'green', 'blue')
+
+    while "Invalid answer":
+        reply = str(input("Perform another transform? (y/n): ")).lower().strip()
+        if reply[:1] == 'y':
+            break
+        if reply[:1] == 'n':
+            sys.exit(0)
